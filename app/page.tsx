89af78040bb946045984896aece02720cca0a51a
Nick Pistorius <nick@tontrac.co.za>
2024-12-17 15:33:53 +0200

    Tailwind CSS Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-
 const Home = () => {
-	return (
-		<div>
-			<h1 className="text-3xl text-white font-black">
-				Welcome to the world of Next.js
-			</h1>
-		</div>
-	);
+    return (
+        <div>
+            <h1 className="text-3xl font-black text-primary-500">
+                Welcome to the world of Next.js
+            </h1>
+        </div>
+    );
 };
 
 export default Home;
